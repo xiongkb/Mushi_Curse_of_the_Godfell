f54c19d3e38514acee14e901c3273b8c855dd3f8
xiongkb <xiong.kang.b@gmail.com>
2022-12-04 11:48:15 -0800

    Fixed glitch

--- Assets/Scripts/Starter Scripts/Managers/CollectibleManager.cs
+++ Assets/Scripts/Starter Scripts/Managers/CollectibleManager.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement; //K: code test
 
 public class CollectibleManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text collectibles;
     [SerializeField] private static int collected;
 
+ //   public static SceneManagement.Scene GetSceneByName();
+  //  private string sceneName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("START");
+        if(SceneManager.GetSceneByName("Level_One").isLoaded){
+            collected = 0;
+            Debug.Log("Collected seeds: " + collected);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log("UPDATE");
         collectibles.text = "Seeds: " + collected;
     }
 
     public void Collected(int value)
     {
-        collected += value;
+        Debug.Log("COLLECTED");
+        // Scene currentScene = SceneManager.GetSceneByName().name; //K: code test
+        if(SceneManager.GetSceneByName("Start").isLoaded){
+            collected = 0;
+            Debug.Log("Collected seeds: " + collected);
+        }
+        // Debug.Log(currentScene);
+       // string sceneName = currentScene.name;
+
+            collected += value;
+        //collected += value;
     }
 
 }
